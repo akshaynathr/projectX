478585100f99bd64932359a94671300b77c19443
akshaynathr <akshaynathr@gmail.com>
2015-04-07 22:16:58 +0530

    Added post page

---
+++ client/post.js
@@ -0,0 +1,33 @@
+$( document ).on( "click", ".show-page-loading-msg", function() {
+    var $this = $( this ),
+        theme = $this.jqmData( "theme" ) || $.mobile.loader.prototype.options.theme,
+        msgText = $this.jqmData( "msgtext" ) || $.mobile.loader.prototype.options.text,
+        textVisible = $this.jqmData( "textvisible" ) || $.mobile.loader.prototype.options.textVisible,
+        textonly = !!$this.jqmData( "textonly" );
+        html = $this.jqmData( "html" ) || "";
+    $.mobile.loading( "show", {
+            text: msgText,
+            textVisible: textVisible,
+            theme: theme,
+            textonly: textonly,
+            html: html
+    });
+})
+.on( "click", ".hide-page-loading-msg", function() {
+    $.mobile.loading( "hide" );
+});
+
+function callme(){
+
+
+var data=document.getElementById("text").value;
+
+$.mobile.showPageLoadingMsg();
+}
+
+/*
+if (data=="")
+{alert("Please enter a message"); }
+else {
+alert(data);
+}*/
