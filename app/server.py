52df32a99f43b8593fbd9c4ee7b90b13be26f484
akshaynathr <akshaynathr@gmail.com>
2015-04-29 11:18:46 +0530

    Edited registration page

--- app/server.py
+++ app/server.py
@@ -3,7 +3,7 @@ from flask.ext.cors import CORS
 
 from flask.ext.mongoengine import MongoEngine
 
-from posts.models import db
+from users.models import db
 
 from flask.ext.restful import Api
 
@@ -11,11 +11,36 @@ from posts.views import Post
 
 from resources import *
 
-app=Flask(__name__,static_url_path='/templates')
+from flask.ext.login import LoginManager,login_user,logout_user,current_user,login_required
+
+
+from mongoengine import *
+
+
+
+
+
+app=Flask(__name__,static_folder='templates')
 app.config["MONGODB_SETTINGS"]={'db':'hoistme'}
+app.secret_key="akshaynathrhoistme"
 api=Api(app)
 cors = CORS(app)
 db.init_app(app)
+
+login_manager=LoginManager()
+login_manager.init_app(app)
+login_manager.login_view='home'
+
+
+
+#login userload function - predefined for flask login
+
+@login_manager.user_loader
+def load_user(mail):
+    t= User.objects(email=mail).first()
+    print(t)
+    return t
+
 
 
 
@@ -23,7 +48,7 @@ db.init_app(app)
 @app.route('/')
 
 def home():
-	return render_template('index.html')
+	return render_template('admin/index.html')
 
 
 #flask blueprint registering
@@ -37,3 +62,44 @@ blueprints()
 
 api.add_resource(Post,'/post')
 
+
+
+
+
+#login
+@app.route('/home', methods=['POST'])
+def login():
+	if request.form['email'] and request.form['password']:
+		email=request.form['email']
+		password=request.form['password']
+		obj=User.objects.filter(Q(email=email) & Q(password=password)).first()
+		
+		if obj:
+			print ("User found. Proceeding login ");
+			login_user(obj)
+		        return "Logged in "			
+		else:	
+			print("User not found . Proceed Registration ");
+			return redirect(url_for('register'))
+
+	return "Enter proper email or username"	
+	
+
+
+
+#logout
+
+@app.route('/logout')
+@login_required
+def logout():
+	logout_user
+	return redirect(url_for('home'))
+
+
+
+#registeration page
+
+@app.route('/register')
+def register():
+	return render_template("registration/index.html")	
+		
