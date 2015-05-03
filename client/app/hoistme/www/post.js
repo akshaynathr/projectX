f74aefeb0000d7c124e13d6d964abbaa512578a1
akshaynathr <akshaynathr@gmail.com>
2015-05-03 11:25:49 +0530

    Added dynamic news creation in ui

--- client/app/hoistme/www/post.js
+++ client/app/hoistme/www/post.js
@@ -4,7 +4,7 @@ function callme()
 	{
 	var data=document.getElementById("text").value;
 	
-	var json_data={"data":data}
+	var json_data={"data":data};
 	if (data==="")
 		{alert("Please enter a message"); }
 	else {
@@ -23,3 +23,64 @@ function callme()
 		     });
 	     }
 	}
+var count=0;
+
+function addnews()
+{   count=count+1;
+    
+    var h2=document.createElement('h2');
+    var h2_txt=document.createTextNode("H2 Testing");
+    h2.appendChild(h2_txt);
+    
+    
+    var p_title=document.createElement('p');
+    p_title.setAttribute('id','title')
+    var txt_title=document.createTextNode("Testing Title");
+    var strong=document.createElement('strong');
+    strong.appendChild(txt_title);
+    p_title.appendChild(strong);
+    
+    var p_message=document.createElement('p');
+    p_message.setAttribute('id','message');
+    var txt_message=document.createTextNode("Testing message");
+    p_message.appendChild(txt_message);
+    
+    
+    
+    var p_time=document.createElement('p');
+    p_time.setAttribute('id','time');
+    p_time.setAttribute('class','ui-li-aside');
+    var txt_time=document.createTextNode('6.24 PM');
+    var strong_time=document.createElement('strong');
+    strong_time.appendChild(txt_time);
+    p_time.appendChild(strong_time);
+    
+    
+    var li_a=document.createElement('a');
+    li_a.setAttribute('href','#');
+    li_a.setAttribute('class','ui-btn ui-btn-icon-right ui-icon-carat-r');
+    li_a.appendChild(h2);
+    li_a.appendChild(p_title);
+    li_a.appendChild(p_message);
+    li_a.appendChild(p_time);
+    
+     //removing last child class from last list during update
+    var lichange=document.getElementById('news_list');
+    var last_li=lichange.childNodes[lichange.childNodes.length-1];
+    last_li.className="";
+    
+    
+    var li=document.createElement('li');
+    li.setAttribute('id','news_1');
+    li.setAttribute('class','ui-last-child');
+    li.appendChild(li_a);
+    
+    var ul=document.getElementById('news_list');
+    ul.appendChild(li);
+    
+    //update the news counter
+    
+    var counter=document.getElementById('count');
+    counter.innerHTML= count.toString();
+    
+}
